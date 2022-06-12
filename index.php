e48f24bddf7f80c2805d97dc4f9c9bb6193a0fa4
MatasRutkauskas <matasrut00@gmail.com>
2022-06-12 15:24:09 +0300

    add routing

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php include_once("privacy-policy.html"); ?>
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/privacy-policy' :
+        require __DIR__ . '/privacy-policy.php';
+        break;
+    case '/support' :
+        require __DIR__ . '/support.php';
+        break;
+    default:
+        http_response_code(404);
+        break;
+}
+?>
